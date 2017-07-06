552e94a059ea07c42cb1226903d8e7bf7bbfd33d
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2017-07-06 11:45:07 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plotsettings',
+      version='1.0',
+      description='Standard Settings for plotting as .pgf',
+      packages=['plotsettings'],
+     )
